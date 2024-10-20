e5435631958390935e4301f890cd2d275808fa83
gitkaviyarasi <kaviyarasikrishnannj@gmail.com>
2024-10-20 14:46:45 -0400

    initial code with the logic for creating read me

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# readmegenerator
-To develop a readme generator using node js
+# readme generator
+
+
+## Description
+this is genreate a reade me with the inputs from the user
+## Table of Contents 
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#Testing)
+* [Questions](#questions)
+
+## Installation
+ji pp 
+
+## Usage
+decc hr ;kpdkclmks nsncjsbcj mnsjdbjsbcj absjbfsj jsbdjbsj
+
+## License
+None
+
+## Contributing
+contribyte the repos nad he] me make the foded better and running and olmlsml nskndkfnsknkf
+
+## Tests
+npm test
+
+## Questions
+#if you have any questions please contact me at 
+
+
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,18 +1,69 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  switch (license) {
+    case 'MIT':
+      return '![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)';
+    case 'Apache 2.0':
+      return '![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)';
+    case 'GPL 3.0':
+      return '![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)';
+    case 'BSD3':
+      return '![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)';
+    default:
+      return '';
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
+function renderLicenseLink(license) {
+  switch (license) {
+    case 'MIT':
+      return 'https://opensource.org/licenses/MIT';
+    case 'Apache 2.0':
+      return 'https://opensource.org/licenses/Apache-2.0';
+    case 'GPL 3.0':
+      return 'https://www.gnu.org/licenses/gpl-3.0';
+    case 'BSD3':
+      return 'https://opensource.org/licenses/BSD-3-Clause';
+    default:
+      return '';
+  }
+}
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+function generateMarkdown(answers) {
+  return `# ${answers.projectName}
+${renderLicenseBadge(answers.license)}
+${renderLicenseLink(answers.license)}
+## Description
+${answers.description}
+## Table of Contents 
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#Testing)
+* [Questions](#questions)
+
+## Installation
+${answers.install}
+
+## Usage
+${answers.usage}
+
+## License
+${answers.license}
+
+## Contributing
+${answers.contribution}
+
+## Tests
+${answers.test}
+
+## Questions
+#if you have any questions please contact me at ${answers.email}
+
 
 `;
 }
